d50aeea7ea6d6ee8d9e77bda0beae6f3cd997a28
ArsuPlayer <danieljimenez@liceovvh.cl>
2026-04-07 12:44:40 -0400

    ola

--- python/fundamentos/fundamentos/01_tipos_de_datos/07_tipo_diccionario.py
+++ python/fundamentos/fundamentos/01_tipos_de_datos/07_tipo_diccionario.py
@@ -27,4 +27,25 @@ if "CRI" in paises: #Preguntamos si existe la clave en el diccionario
 else: #No existe esa clave
     paises["CRI"] = "Costa Rica"
 
+#Eliminar valores del diccionario
+valor_removido = paises.pop("MEX") #Elimina el elemento y devuelve su valor
+print(f"Valor removido: {valor_removido}")
+del paises["COL"] #Elimina el elemento
+print(paises) #Imprime: {'CHL': 'Chile'}
 
+#Diccionario pintor
+pintor = {
+    "nombre": "Frida Kahlo",
+    "pais": "México",
+    "fecha_nacimiento": "6 de julio de 1907"
+}
+
+#Diccionario anidado
+escuela = {
+    "nombre": "Coding Dojo LATAM",
+    "profesores": [
+        {"nombre": "Alfredo", "apellido": "Salazar", "cursos": ["Python", "Java"]},
+        {"nombre": "Valeria", "apellido": "Romero", "cursos": ["Fundamentos", "Java"]},
+        {"nombre": "Marcelo", "apellido": "Argotti", "cursos":["MERN", "Python"]}
+    ]
+}
