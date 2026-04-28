3d8e2b4e46ad466f6d39a3e24d58705e56bc810a
Daniel <danieljimenez@liceovvh.cl>
2026-04-28 12:57:54 -0400

    Update 4.-usuario_streaming.py

--- python/fundamentos/oop/4.-usuario_streaming.py
+++ python/fundamentos/oop/4.-usuario_streaming.py
@@ -33,28 +33,40 @@ class UsuarioStreaming:
     def agregar_a_lista(self, titulo):
         """Agrega un contenido a la lista de reproducción del usuario."""
         self.lista_reproduccion.append(titulo)
-        print(f"Usuario {self.lista_reproduccion}")
-        pass
+        print(f"Titulo '{titulo}' agregado correctamente")
 
 
     def ver_contenido(self, titulo):
         """Simula que el usuario reproduce un contenido."""
-        pass
+        if titulo in self.lista_reproduccion:
+            print(f"EL usuario {self.nombre} esta viendo '{titulo}' ")
+        else:
+            print(f"Titulo no encontrado {titulo}") 
 
 
     def cambiar_suscripcion(self, nueva_suscripcion):
         """Cambia el tipo de suscripción del usuario."""
-        pass
+        susAntigua = self.suscripcion
+        self.suscripcion = nueva_suscripcion
+        print(f"Suscripcion cambio de {susAntigua} a {nueva_suscripcion}")
 
 
     def mostrar_info_usuario(self):
         """Muestra la información del usuario y su lista de reproducción."""
-        pass
-
+        print(f"Nombre: {self.nombre} ")
+        print(f"Email: {self.email}")
+        print(f"Suscripcion: {self.suscripcion}")
+        if len(self.lista_reproduccion) == 0:
+            print("La lista de reproduccion esta vacia")
+        else:
+            print(f"Lista de reproduccion: \n- {"\n-".join(self.lista_reproduccion)} ")
+        
+# Instancias
+daniel = UsuarioStreaming("daniel", "danieljimenez@gmail.com")
+daniel.agregar_a_lista("La casa de papel")
+daniel.cambiar_suscripcion("premium")
+daniel.ver_contenido("La casa de papel")
+daniel.mostrar_info_usuario()
 # Todos los valores que se deban registrar debe ser con input
 # Añadir un menu While para llamar a los métodos
-usuario1 = UsuarioStreaming("usuario123", "user123@gmail.com" )
-usuario2 = UsuarioStreaming("usuario321", "user321@gmail.com" )
-usuario3 = UsuarioStreaming("usuario12345", "user12345@gmail.com")
-
 
